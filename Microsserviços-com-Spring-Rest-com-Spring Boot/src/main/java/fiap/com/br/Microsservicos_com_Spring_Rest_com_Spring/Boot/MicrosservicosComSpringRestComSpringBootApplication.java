d1dfa8147388a322ddba0a58308283c547d0188d
• Jennifer Daleffi • <161328492+JenniferDaleffi@users.noreply.github.com>
2025-05-08 22:08:20 -0300

    Add files via upload

---
+++ Microsserviços-com-Spring-Rest-com-Spring Boot/src/main/java/fiap/com/br/Microsservicos_com_Spring_Rest_com_Spring/Boot/MicrosservicosComSpringRestComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.Microsservicos_com_Spring_Rest_com_Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicosComSpringRestComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicosComSpringRestComSpringBootApplication.class, args);
+	}
+
+}
